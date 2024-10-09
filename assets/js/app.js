39ccf97b22538e57833d827e3345a4a3eaf0b960
unknown <you@example.com>
2024-10-09 21:14:22 +0530

    breakpoints used for responsive slick slider in js

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,46 @@ $(document).ready(function () {
       speed: 500,
       slidesToShow: 3,
       slidesToScroll: 2,
+      //  autoplay:true,
+      //  autoplaySpeed: 2000,
       arrows:false,
-   });
+      responsive: [
+        {
+        breakpoint: 1024,
+        settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        // dots: true,
+        arrows:true,
+        // dotsClass: 'custom-dots',
+        }
+        },
+        {
+        breakpoint: 768,
+        settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        // dots: true,
+        arrows:true,
+        // dotsClass: 'custom-dots',
+        }
+        },
+
+        {
+          breakpoint: 576,
+          settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          // dots: true,
+          arrows:true,
+          // dotsClass: 'custom-dots',
+          }
+          },
+
+        ]
+        });
 });
 
