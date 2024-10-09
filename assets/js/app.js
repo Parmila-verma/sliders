2e7b9bab9b1de1b2a1b047523aaa2bee84a9db4c
unknown <you@example.com>
2024-10-09 14:36:05 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $('.slider').slick({
+      dots: true,
+      dotsClass: 'custom-dots',
+      infinite: true,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 2,
+      arrows:false,
+   });
+});
+
