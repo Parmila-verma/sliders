b2b07f0639cf8a29b42d6789277fec1d559390e6
unknown <you@example.com>
2024-10-11 14:06:28 +0530

    animation added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,51 +1,52 @@
 $(document).ready(function () {
   $('.slider').slick({
-      dots: true,
-      dotsClass: 'custom-dots',
-      infinite: true,
-      speed: 500,
-      slidesToShow: 3,
-      slidesToScroll: 2,
-      //  autoplay:true,
-      //  autoplaySpeed: 2000,
-      arrows:false,
-      responsive: [
-        {
+    // centerMode:true,
+    dots: true,
+    dotsClass: 'custom-dots',
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    //  autoplay:true,
+    //   autoplaySpeed: 2000,
+    arrows: false,
+    responsive: [
+      {
         breakpoint: 1024,
         settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        infinite: true,
-        // dots: true,
-        arrows:true,
-        // dotsClass: 'custom-dots',
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          // arrows: false,
+          dotsClass: 'custom-dots',
         }
-        },
-        {
+      },
+      {
         breakpoint: 768,
         settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        infinite: true,
-        // dots: true,
-        arrows:true,
-        // dotsClass: 'custom-dots',
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          // arrows: false,
+          dotsClass: 'custom-dots',
         }
-        },
+      },
 
-        {
-          breakpoint: 576,
-          settings: {
+      {
+        breakpoint: 576,
+        settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
           infinite: true,
-          // dots: true,
-          arrows:true,
-          // dotsClass: 'custom-dots',
-          }
-          },
+          dots: true,
+          // arrows: false,
+          dotsClass: 'custom-dots',
+        }
+      },
 
-        ]
-        });
+    ]
+  });
 });
 
